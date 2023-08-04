0c4404e8e90127abddf8465e41732a80adf3ac99
Akin Campbell <54187807+akincampbell@users.noreply.github.com>
2023-08-04 14:07:38 +0000

    Add affirmations and homepage

--- app.rb
+++ app.rb
@@ -2,8 +2,28 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/aff") do
+  erb(:new_aff)
+end
+
+get("/aff/results") do
+  affirmations = [
+    "This is what I can handle right now, and that is okay",
+    "I haven't figured it out yet",
+    "Now that I know I can do better",
+    "This is what I can handle right now",
+    "My body does not define my worth",
+    "I am allowed to make mistakes",
+    "Some day are harder than others: I am doing my best today"
+  ]
+  i = rand(0..6)
+  @output = affirmations[i]
+  erb(:aff_results)
+end
+
+get("/facts") do
+  erb(:homepage)
 end
